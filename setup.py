1ed6336d8e5920edc4784737538eaab6ca287fe5
Niranjan Rajendran <niranjan94@yahoo.com>
2016-10-26 22:50:33 +0530

    Version bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author='Niranjan Rajendran',
     author_email='niranjan94@yahoo.com',
     url='https://github.com/niranjan94/python-pentabarf-xml',
-    download_url='https://github.com/niranjan94/python-pentabarf-xml/tarball/0.1',
+    download_url='https://github.com/niranjan94/python-pentabarf-xml/tarball/0.3',
     keywords=['xml', 'pentabarf', 'conference', 'schedule'],
     classifiers=[],
 )
