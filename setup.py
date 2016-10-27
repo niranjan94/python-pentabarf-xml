2bb9e6b75b41764ef2cf69c27f9173ef6808a692
Niranjan Rajendran <niranjan94@yahoo.com>
2016-10-27 19:46:31 +0530

    Fix

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name='python-pentabarf-xml',
     packages=['pentabarf'],  # this must be the same as the name above
-    version='0.132',
+    version='0.133',
     description='Generate pentabarf XML schedule for events/conferences',
     author='Niranjan Rajendran',
     author_email='niranjan94@yahoo.com',
     url='https://github.com/niranjan94/python-pentabarf-xml',
-    download_url='https://github.com/niranjan94/python-pentabarf-xml/tarball/0.132',
+    download_url='https://github.com/niranjan94/python-pentabarf-xml/tarball/0.133',
     keywords=['xml', 'pentabarf', 'conference', 'schedule'],
     classifiers=[],
 )
