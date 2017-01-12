d54a30054fc3dbf52f9c0743d56880455c051512
Niranjan Rajendran <niranjan94@yahoo.com>
2017-01-12 14:32:18 +0530

    Version bump to 0.19

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name='python-pentabarf-xml',
     packages=['pentabarf'],
-    version='0.18',
+    version='0.19',
     description='Generate pentabarf XML schedule for events/conferences',
     author='Niranjan Rajendran',
     author_email='niranjan94@yahoo.com',
     url='https://github.com/niranjan94/python-pentabarf-xml',
-    download_url='https://github.com/niranjan94/python-pentabarf-xml/tarball/0.18',
+    download_url='https://github.com/niranjan94/python-pentabarf-xml/tarball/0.19',
     keywords=['xml', 'pentabarf', 'conference', 'schedule'],
     classifiers=[], requires=['defusedxml']
 )
